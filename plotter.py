aaaa389e53ddf8054fb0d8e07dc9fe07f909975f
stonexis <pshelepov@yandex.ru>
2024-12-03 23:56:49 +0300

    add seaborn

--- plotter.py
+++ plotter.py
@@ -1,11 +1,12 @@
 import json
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 def flatten_2d_array(array_2d):
     """Функция для преобразования двумерного массива в одномерный"""
     return [item for sublist in array_2d for item in sublist]
 
-def plot_graph(filename):
+def plot_graph_seaborn(filename):
     # Чтение JSON данных из файла
     with open(filename, 'r') as file:
         data = json.load(file)
@@ -17,24 +18,38 @@ def plot_graph(filename):
     array_fx = flatten_2d_array(data['array_fx'])
     array_approximation = flatten_2d_array(data['array_approximation'])
 
+    # Установка темы и стиля
+    sns.set_theme(style="darkgrid", palette="pastel", font="serif", font_scale=1.2)
+
     # Построение графика
-    plt.figure(figsize =(15,15))
-    plt.minorticks_on()
-    plt.grid(which='major')
-    plt.grid(which='minor')
+    plt.figure(figsize=(14, 8))
 
-    # Построение линий
-    plt.plot(array_x, array_fx, color='blue', linewidth=2, label='fn')  # Синие линии для исходной функции
-    plt.plot(array_x, array_approximation, 'r--', linewidth=1.5, label='L')  # Красные линии для аппроксимации
+    # Построение линий для исходной функции и аппроксимации
+    sns.lineplot(x=array_x, y=array_fx, label='f(x)', color='darkred', linewidth=2, zorder=1)
+    sns.lineplot(x=array_x, y=array_approximation, label='L(x)', color='red', linestyle='--', linewidth=1.5, zorder=2)
 
-    # Отображение точек интерполяции
-    plt.plot(array_nodes, array_f_nodes, 'gs', markersize=3, label='Points')
+    # Добавление точек интерполяции
+    sns.scatterplot(x=array_nodes, y=array_f_nodes, color='darkred', s=40, label='Points', marker='o', zorder=3)
 
-    # Настройки осей и легенды
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.legend()
+    # Настройки графика
+    plt.xlabel("X", fontsize=14)
+    plt.ylabel("Y", fontsize=14)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+
+    # Настройка сетки
+    plt.grid(which='major', linestyle='-', linewidth=0.5, color='gray')  # Основная сетка
+    plt.grid(which='minor', linestyle=':', linewidth=0.5, color='gray')  # Мелкая сетка
+    plt.minorticks_on()  # Включение дополнительной сетки
+    plt.tight_layout()
+
+    # Легенда
+    plt.legend(fontsize=12, loc="best", fancybox=True, shadow=True)
+
+    # Отображение графика
     plt.show()
 
+# Вызов функции
+plot_graph_seaborn("data.json")
 
-plot_graph("data.json")
+
