9e9443b0064eddcc4a9b521e88f37715124d8db8
stonexis <pshelepov@yandex.ru>
2024-12-04 10:50:10 +0300

    refactor sns

--- plotter.py
+++ plotter.py
@@ -19,17 +19,17 @@ def plot_graph_seaborn(filename):
     array_approximation = flatten_2d_array(data['array_approximation'])
 
     # Установка темы и стиля
-    sns.set_theme(style="darkgrid", palette="pastel", font="serif", font_scale=1.2)
+    sns.set_theme(style="whitegrid", palette="muted", font="serif", font_scale=1.2)
 
     # Построение графика
     plt.figure(figsize=(14, 8))
 
     # Построение линий для исходной функции и аппроксимации
-    sns.lineplot(x=array_x, y=array_fx, label='f(x)', color='darkred', linewidth=2, zorder=1)
-    sns.lineplot(x=array_x, y=array_approximation, label='L(x)', color='red', linestyle='--', linewidth=1.5, zorder=2)
+    sns.lineplot(x=array_x, y=array_fx, label='f(x)', linewidth=2, zorder=1)
+    sns.lineplot(x=array_x, y=array_approximation, label='L(x)', linestyle='--', linewidth=1.5, zorder=2)
 
     # Добавление точек интерполяции
-    sns.scatterplot(x=array_nodes, y=array_f_nodes, color='darkred', s=40, label='Points', marker='o', zorder=3)
+    sns.scatterplot(x=array_nodes, y=array_f_nodes, s=40, label='Points', marker='o', zorder=3)
 
     # Настройки графика
     plt.xlabel("X", fontsize=14)
@@ -38,13 +38,13 @@ def plot_graph_seaborn(filename):
     plt.yticks(fontsize=12)
 
     # Настройка сетки
-    plt.grid(which='major', linestyle='-', linewidth=0.5, color='gray')  # Основная сетка
-    plt.grid(which='minor', linestyle=':', linewidth=0.5, color='gray')  # Мелкая сетка
+    plt.grid(which='major', linestyle='-', linewidth=0.5)  # Основная сетка
+    plt.grid(which='minor', linestyle=':', linewidth=0.5)  # Мелкая сетка
     plt.minorticks_on()  # Включение дополнительной сетки
     plt.tight_layout()
 
     # Легенда
-    plt.legend(fontsize=12, loc="best", fancybox=True, shadow=True)
+    plt.legend(fontsize=12, loc="best", fancybox=True)
 
     # Отображение графика
     plt.show()
